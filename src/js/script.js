9145ce9392d084ecdba050062a67bccc8bcad294
Louis <E222149R>
2024-10-13 01:04:39 +0200

    box géré js et image def

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,62 @@
 
 $(document).ready(function(){
 
+    //ralentir la video
+    $('#backgroundVideo')[0].playbackRate=0.8;
+
 
     //sur les sections de textes à faire apparaître
-    $('.textToHide').click(function (e){
+    $('.sub-division').click(function (){
 
-        $('.textToShow').css('display','flex'); 
+       if( $('.textToHide').css('display')!='none'){
+
         $('.textToHide').css("display","none");
+        $('.textToShow').css("display","flex");
+
+        $('.sub-division').css({
+            'font-size':'20px',
+            'background-color':'black',
+            'color':'white'}
+        )
+
+       }; 
+      
+    })
+
+
+    //Gérer  les entrées dans les divisions de textes:
+    //si le texte est affiché, on ne veut pas de style lors du hover
+    $('.sub-division').on('mouseenter',function (){
+        
+        if ($('.textToShow').css('display')=='none'){
+        
+            $(this).css({
+                'font-size':'25px',
+                'background-color':'rgb(26, 0, 39)',
+                'color':'white'}
+            )
+        }
+    })
+
+    $('.sub-division').on('mouseleave',function (){
+        
+        if ($('.textToShow').css('display')=='none'){
+            $(this).css({
+                'font-size':'20px',
+                'background-color':'black',
+                'color':'white'}
+            )
+        }
 
     })
 
-    //ralentir la video
-    $('#backgroundVideo')[0].playbackRate=0.8;
-    
+    $('.return-section').on('click',function(e){
+        //la sub-division a déjà un onclick, le bouton cliqué, cela se propage aussi sur la box, ce n'est pas ce qu'on veut.
+        e.stopPropagation()
 
+        $('.textToShow').css('display','none'); 
+        $('.textToHide').css("display","flex");
+    })
+})
 
-});
+//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
