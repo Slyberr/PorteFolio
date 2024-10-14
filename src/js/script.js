b453626c91cf3e7cd58d7020e260df5ef9da8754
E222149R <E222149R@etu.univ-nantes.fr>
2024-10-14 15:40:16 +0200

    entete fonctionnelle

--- src/js/script.js
+++ src/js/script.js
@@ -1,54 +1,40 @@
+//sur les sections de textes à faire apparaître
+function showHidTextSubDivTreatement(currentID){
+ 
+    //si le texte est affiché, on ne veut pas de style lors du hover
+          
+    if( $(currentID).find('.textToHide').css('display')!='none' ){
+
+        $(currentID).find('.textToHide').css("display","none");
+        $(currentID).find('.textToShow').css("display","flex");
+
+        $(currentID).css({
+            'font-size':'20px',
+            'background-color':'black',
+            'color':'white'}
+        )
+    }
+
+  
+}
 
 $(document).ready(function(){
 
     //ralentir la video
     $('#backgroundVideo')[0].playbackRate=0.8;
 
-
-    //sur les sections de textes à faire apparaître
-    $('.sub-division').click(function (){
-
-       if( $('.textToHide').css('display')!='none' ){
-
-        $(this).find('.textToHide').css("display","none");
-        $(this).find('.textToShow').css("display","flex");
-
-        $(this).css({
-            'font-size':'20px',
-            'background-color':'black',
-            'color':'white'}
-        )
-
-       }; 
-      
-    })
-
-
-    //Gérer  les entrées dans les divisions de textes:
-    //si le texte est affiché, on ne veut pas de style lors du hover
-    $('.sub-division').on('mouseenter',function (){
-        
-        if ($(this).find('.textToShow').css('display')=='none'){
-        
-            $(this).css({
-                'font-size':'25px',
-                'background-color':'rgb(26, 0, 39)',
-                'color':'white'}
-            )
-        }
-    })
-
-    $('.sub-division').on('mouseleave',function (){
-        
-        if ($(this).find('.textToShow').css('display')=='none'){
-            $(this).css({
-                'font-size':'20px',
-                'background-color':'black',
-                'color':'white'}
-            )
-        }
+    //code pour l'entête
+    $('.entete').on('click',function(){
+        let sectionName =$(this).attr('href')
+        showHidTextSubDivTreatement(sectionName)
 
     })
+
+    $('.sub-division').on('click',function(){
+        showHidTextSubDivTreatement('#'+ $(this).attr('id'))
+
+    })
+    
 
     $('.return-section').on('click',function(e){
         //la sub-division a déjà un onclick, le bouton cliqué, cela se propage aussi sur la box, ce n'est pas ce qu'on veut.
